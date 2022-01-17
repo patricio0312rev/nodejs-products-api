2a908a672a6887d20321f15833e625ead46ea11e
patricio0312rev <review0312oficial@gmail.com>
2022-01-16 23:15:00 -0500

    Definiendo metodo ProductController@patchById

--- controllers/products.js
+++ controllers/products.js
@@ -26,6 +26,26 @@ class ProductController {
     async updateById (req, res) {
         res.status(405).json({ message: "Not allowed" });
     }
+
+    async patchById (req, res) {
+        const { id } = req.params;
+        const { body } = req;
+        const data = await ProductModel.findOne({where: {id}});
+        if (!data) {
+            res.status(404).send();
+            return;
+        }
+        const { price, mrp, stock } = data;
+        if(mrp < 5 && stock == 0) {
+            res.status(422).send([]);
+        } else if (mrp < price) {
+            res.status(422).send([]);
+        } else if (stock == 0) {
+            res.status(422).send([]);
+        } else {
+            await ProductModel.update({isPublished: true}, {where: {id}});
+        }
+    }
 }
 
 module.exports = new ProductController();
