1e1be8366b9cd120b1c09f8995620b7e5831b18a
patricio0312rev <review0312oficial@gmail.com>
2022-01-16 23:13:50 -0500

    Definiendo metodo ProductController@create

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,18 @@
 const Products = require('../models/products');
 
 class ProductController {
-
+    async create(req, res) {
+        try {
+            const { body } = req;
+            const data = await ProductModel.findAll({});
+            body.id = data.length + 1;
+            body.isPublished = false;
+            const response = await ProductModel.create(body);
+            res.status(201).json(response);
+        } catch (err) {
+            throw new Error(err.message);
+        }
+    }
 }
 
 module.exports = new ProductController();
